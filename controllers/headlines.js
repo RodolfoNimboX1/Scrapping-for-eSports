e0ad92e8a5d17e113fce4ef54eed75007dbe5c1b
Rodolfo Resines <rodolfo.sresines@gmail.com>
2018-09-03 19:28:14 -0500

    created the app for nyt scraper, still working on some bugs

---
+++ controllers/headlines.js
@@ -0,0 +1,36 @@
+var scrape = require("../scripts/scrape");
+var makeDate = require("../scripts/date");
+
+var Headline = require("../models/Headline");
+
+module.exports = {
+    fetch: function(cb) {
+        scrape(function(data) {
+            var atricles = data;
+            for (var i=0; i<atricles.length; i++) {
+                atricles[i].date = makeDate();
+                atricles[i].saved = false;
+            }
+            Headline.collection.insertMany(atricles, {ordered:false}, function(err, docs) {
+                cb(err, docs);
+            });
+        });
+    },
+    delete: function(query, cb) {
+        Headline.remove(query, cb);
+    },
+    get: function(query, cb) {
+        Headline.find(query)
+        .sort({
+            _id: -1
+        })
+        .exec(function(err, doc) {
+            cb(doc);
+        });
+    },
+    update: function(query, cb) {
+        Headline.update({_id: query._id}, {
+          $set: query  
+        }, {}, cb);
+    }
+}
--- server.js
+++ server.js
@@ -2,16 +2,26 @@ var express = require("express");
 var handlebars = require("express-handlebars");
 var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
-var request = require("request");
-var cheerio = require("cheerio");
 
+var PORT = process.env.PORT || 8080;
 // Initialize Express
 var app = express();
+var router = express.Router();
 
-// Use body-parser for handling form submissions
-app.use(bodyParser.urlencoded({ extended: true }));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
+
+// Use Handlebars
+app.engine("handlebars", handlebars({
+  defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Every request goes thru router
+app.use(router);
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
@@ -21,9 +31,8 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 
-
 // Routes
-
+require("./config/routes")(router);
 
 // Start the server
 app.listen(PORT, function() {
